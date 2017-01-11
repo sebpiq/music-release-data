bfeb5a61d283c5159df00ffd2dbca482a6648955
Sébastien Piquemal <sebpiq@gmail.com>
2017-01-11 19:30:06 +0200

    fixed soundcloud scraper

--- compile_data/soundcloud_release_per_year.py
+++ compile_data/soundcloud_release_per_year.py
@@ -2,10 +2,20 @@ import urllib
 import os
 import json
 
+import settings
 import common
 
+def _make_url(date_start, date_end):
+    query = {}
+    query['client_id'] = settings.soundcloud_client_id
+    query['created_at[from]'] = date_start 
+    query['created_at[to]'] = date_end
+    query['limit'] = 51
+    return ( 'https://api.soundcloud.com/tracks.json?%s' 
+        % '&'.join(['%s=%s' % (k, v) for k, v in query.items()]) )
 
-for year in range(2000, 2017):
+
+for year in range(2006, 2017):
     print('starting year %s' % year)
     json_path = os.path.join(common.data_folder, '%s-soundcloud.json' % year)
 
@@ -20,31 +30,37 @@ for year in range(2000, 2017):
         seen_releases = {
             'total_duration': 0,
             'total_tracks': 0,
-            'next_href': None,
-            'last_date': None
+            'last_date': None,
+            'last_ids': []
         }
 
-    if not seen_releases['next_href']:
-        query = {}
-        query['client_id'] = ''
-        query['created_at[from]'] = '%s-01-01 00:00:00' % year
-        query['created_at[to]'] = '%s-12-31 23:59:59' % year
-        query['linked_partitioning'] = 1
-        seen_releases['next_href'] = ( 'https://api.soundcloud.com/tracks.json?%s' 
-            % '&'.join(['%s=%s' % (k, v) for k, v in query.items()]) )
-
     while True:
-        results = common.safe_get(seen_releases['next_href'])
-        for track_dict in results['collection']:
+        # Building url, starting from last date if there is one
+        if seen_releases['last_date']:
+            search_url = _make_url('%s-01-01 00:00:00' % year, seen_releases['last_date'])
+        else:
+            search_url = _make_url('%s-01-01 00:00:00' % year, '%s-12-31 23:59:59' % year)
+        results = common.safe_get(search_url)
+
+        # Removing duplicates
+        if seen_releases['last_ids'] and results:
+            cleaned = []
+            for track_dict in results:
+                if not track_dict['id'] in seen_releases['last_ids']:
+                    cleaned.append(track_dict)
+            results = cleaned
+        if not results: break
+
+        # Counting tracks
+        for track_dict in results:
             seen_releases['total_duration'] += track_dict['duration'] / 1000
             seen_releases['total_tracks'] += 1
+            seen_releases['last_ids'].append(track_dict['id'])
 
-        seen_releases['next_href'] = results.get('next_href', None)
-        if results['collection']:
-            seen_releases['last_date'] = results['collection'][-1]['created_at']
+        seen_releases['last_date'] = results[-1]['created_at']
+        if len(seen_releases['last_ids']) > 2000:
+            seen_releases['last_ids'] = seen_releases['last_ids'][2000:]
         _backup_results()
 
-        if not results.get('next_href', None):
-            break
         common.print_progress('year %s | duration %s \t\t\t | tracks %s                ' 
             % (year, seen_releases['total_duration'], seen_releases['total_tracks']))
